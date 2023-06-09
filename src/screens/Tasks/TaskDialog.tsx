8c7fe91332ce6063845164240bb35159c4a0227f
maynard <33montefalconyoyon@gmail.com>
2023-06-09 11:33:10 +0800

    Implement TaskForm interface

--- src/screens/Tasks/TaskDialog.tsx
+++ src/screens/Tasks/TaskDialog.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {Dialog, DialogTitle} from "@mui/material";
 import {Dispatch} from "react";
+import TaskForm from "./TaskForm";
 
 type TaskDialogProps = {
     show: boolean;
@@ -9,9 +10,11 @@ type TaskDialogProps = {
 }
 
 const TaskDialog = ({show, setShowDialog, actionFilter}: TaskDialogProps): JSX.Element => {
+    let action = actionFilter ? "Add" : "Edit";
     return (
-        <Dialog open={show} onClose={() => setShowDialog(false)}>
-            <DialogTitle>{actionFilter} Task</DialogTitle>
+        <Dialog open={show} onClose={() => setShowDialog(false)} fullWidth>
+            <DialogTitle>{action} Task</DialogTitle>
+            <TaskForm actionFilter={actionFilter}/>
         </Dialog>
     );
 };
--- src/screens/Tasks/TaskForm.tsx
+++ src/screens/Tasks/TaskForm.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react';
+import {Box, Button, DialogContent, TextField} from "@mui/material";
 
-const TaskForm = () => {
+const TaskForm = (actionFilter: boolean): JSX.Element => {
+    // console.log(actionFilter)
     return (
-        <div>
-
-        </div>
+        <DialogContent component="form" noValidate sx={{mt: 1}}>
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="taskName"
+                label="Task"
+                name="taskName"
+                autoFocus
+            />
+            <Button type="submit" fullWidth variant="contained" sx={{mt: 3, mb: 2}}>
+                Add Task
+            </Button>
+        </DialogContent>
     );
 };
 

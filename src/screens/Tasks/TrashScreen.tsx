1e33751453b4517e3bf5611f80ff14bf2ab51acd
maynard <33montefalconyoyon@gmail.com>
2023-06-09 08:44:11 +0800

    Implement trashscreen

---
+++ src/screens/Tasks/TrashScreen.tsx
@@ -0,0 +1,13 @@
+// @flow 
+import * as React from 'react';
+
+const TrashScreen = () => {
+    // TODO:: Create trash screen
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default TrashScreen;

372a1f6e54011aeacc806b156d665768b7285b66
maynard <33montefalconyoyon@gmail.com>
2023-06-10 15:01:35 +0800

    Implement default value

--- src/screens/Tasks/TaskScreen.tsx
+++ src/screens/Tasks/TaskScreen.tsx
@@ -7,26 +7,29 @@ import {config} from "../../config";
 import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 import TaskDialog from "./TaskDialog";
 
-const TaskScreen = (): JSX.Element => {
-    const[tasks, setTasks] = useState({});
-    const[show, setShowDialog] = useState<boolean>(false);
-    const [task, setTask] = useState<Task>(
-        {
-            id: "",
-            attributes: {
-                name: ""
-            },
-            relationships: {
-                statuses: {
-                    attributes: {
-                        statusName: ""
-                    }
+const defaultValue = () => {
+    return {
+        id: "",
+        attributes: {
+            name: ""
+        },
+        relationships: {
+            statuses: {
+                attributes: {
+                    statusName: ""
                 }
             }
         }
-    );
+    }
+};
+
+const TaskScreen = (): JSX.Element => {
+    const[tasks, setTasks] = useState({});
+    const[show, setShowDialog] = useState<boolean>(false);
+    const[task, setTask] = useState<Task>(defaultValue());
     const[actionFilter, setActionFilter] = useState<boolean>(false);
 
+    console.log(task)
     useEffect(() => {
         const fetchTasks = async () => {
             try {

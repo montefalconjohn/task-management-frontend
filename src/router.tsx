fdc94d71f4cf72646bba04fd80ed6a85528f6275
maynard <33montefalconyoyon@gmail.com>
2023-06-23 18:36:00 +0800

    Move Protected and Unauthenticatedlayout into a directory

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import {createBrowserRouter} from "react-router-dom";
-import ProtectedLayout from "./Components/ProtectedLayout";
-import UnauthenticatedLayout from "./Components/UnauthenticatedLayout";
+import ProtectedLayout from "./Components/Layout/ProtectedLayout";
+import UnauthenticatedLayout from "./Components/Layout/UnauthenticatedLayout";
 import Login from "./screens/Login/Login";
 import TaskScreen from "./screens/Tasks/TaskScreen";
 

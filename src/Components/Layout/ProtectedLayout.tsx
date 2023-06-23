614c76163851d29a1232c4234bd68da3d51dabc2
maynard <33montefalconyoyon@gmail.com>
2023-06-23 18:37:04 +0800

    Add return value for ProtectedLayout and UnauthenticatedLayout component

--- src/Components/Layout/ProtectedLayout.tsx
+++ src/Components/Layout/ProtectedLayout.tsx
@@ -1,9 +1,9 @@
 // @flow 
 import * as React from 'react';
 import {Container} from "@mui/material";
-import {Navigate, Outlet} from "react-router-dom";
+import {Outlet} from "react-router-dom";
 
-const ProtectedLayout = () => {
+const ProtectedLayout = (): JSX.Element => {
     return (
         <Container maxWidth="lg" sx={{marginTop: "20px"}}>
             <Outlet/>

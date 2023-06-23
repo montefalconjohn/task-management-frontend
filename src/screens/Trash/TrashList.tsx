c8d1ec2a65605bfa34d23b64ea7f46f1806b1750
maynard <33montefalconyoyon@gmail.com>
2023-06-23 19:32:36 +0800

    Implement TrashList

---
+++ src/screens/Trash/TrashList.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import {Task} from "../../services/models/Task";
+import {IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+
+type TrashListProp = {
+    tasks: {}[];
+    onRestoreClick: (item: Task) => void;
+    onDeleteClick: (item: Task) => void;
+}
+
+export const DisplayList = ({tasks, onRestoreClick, onDeleteClick}: TrashListProp) => {
+    return (
+        <>
+            {
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Task</TableCell>
+                                <TableCell colSpan={3}>Status</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                tasks.length >= 1 && tasks.map((item: Task, index) => {
+                                        return (
+                                            <TableRow key={item.id}>
+                                                <>
+                                                    <TableCell>{item.attributes.name}</TableCell>
+                                                    <TableCell
+                                                    >{item.relationships.statuses.attributes.statusName}</TableCell>
+                                                    <TableCell>
+                                                        <IconButton aria-label="edit" onClick={() => onRestoreClick(item)}
+                                                                    sx={{align: "right"}}>
+                                                            <EditIcon color="success"/>
+                                                        </IconButton>
+                                                    </TableCell>
+                                                    <TableCell>
+                                                        <IconButton aria-label="edit" onClick={() => onDeleteClick(item)}>
+                                                            <DeleteForeverIcon color="error"/>
+                                                        </IconButton>
+                                                    </TableCell>
+                                                </>
+                                            </TableRow>
+                                        )
+                                    }
+                                )
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            }
+        </>
+    )
+};
+
+const TrashList = ({tasks, onRestoreClick, onDeleteClick}: TrashListProp): JSX.Element => {
+    return (
+        <>
+            {
+                tasks.length < 1 ?
+                    <div style={{textAlign: "center"}}><p>Results not found.</p></div> :
+                    DisplayList({tasks, onRestoreClick, onDeleteClick})
+            }
+        </>
+    );
+};
+
+export default TrashList;
--- src/screens/Trash/TrashScreen.tsx
+++ src/screens/Trash/TrashScreen.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {config} from "../../config";
+import {Box} from "@mui/material";
+import TrashList from "./TrashList";
 
 const TrashScreen = (): JSX.Element => {
     const[trashTasks, setTasks] = useState([]);
@@ -9,7 +11,7 @@ const TrashScreen = (): JSX.Element => {
     useEffect(() => {
         const fetchTasks = async () => {
             try {
-                const document = await axios.get(`${config.apiBaseUrl}/trash-tasks  `);
+                const document = await axios.get(`${config.apiBaseUrl}/trash-tasks`);
                 const {data} = document;
                 setTasks(data.data)
             } catch (err: any) {
@@ -20,9 +22,24 @@ const TrashScreen = (): JSX.Element => {
         fetchTasks();
     }, []);
 
-    console.log(trashTasks)
+    const onRestoreClick = () => {
+        console.log('Restore');
+    };
+
+    const onDeleteClick = () => {
+        console.log('Delete');
+    };
+
     return (
-        <h1>Trash</h1>
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column'
+        }}>
+            <h2>Trash List</h2>
+            <TrashList tasks={trashTasks} onRestoreClick={onRestoreClick} onDeleteClick={onDeleteClick}/>
+        </Box>
     );
 };
 
